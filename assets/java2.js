ec740ea845d5a04b53b80de89bf979f09d582e72
lmedinatriana <miguemetri@gmail.com>
2017-05-24 10:58:41 -0400

    Added Site to github

---
+++ assets/java2.js
@@ -0,0 +1,325 @@
+jQuery(function ($) {
+    var $paragraphs = $('.menu-toggle');
+   var $background = $('.expand-bg').add('.triangle');
+    $('.menu-item').click(function () {
+    $background.addClass('show-expand');
+        var $target = $($(this).data('target')).stop(true).slideToggle(300, function() {
+      $.each($paragraphs, function(index, value) { 
+        if($paragraphs.filter(':visible').length === 0) {
+          $background.removeClass('show-expand');
+          }
+        });
+
+    });
+        
+        $paragraphs.not($target).filter(':visible').stop(true, true).slideUp(300);
+    
+  });
+
+    $('.menu-item-1').click(function() {
+        $('.triangle').css({
+        'left' : '13%' //moves left
+        });
+    });
+  
+  $('.menu-item-2').click(function() {
+        $('.triangle').css({
+        'left' : '50%' //moves left
+        });
+    });
+  
+  $('.menu-item-3').click(function() {
+        $('.triangle').css({
+        'left' : '87%' //moves left
+        });
+    });
+    
+});
+
+String.prototype.rightChars = function(n){
+  if (n <= 0) {
+    return "";
+  }
+  else if (n > this.length) {
+    return this;
+  }
+  else {
+    return this.substring(this.length, this.length - n);
+  }
+};
+
+(function($) {
+  var
+    options = {
+      highlightSpeed    : 20,
+      typeSpeed         : 100,
+      clearDelay        : 500,
+      typeDelay         : 200,
+      /*clearOnHighlight  : false,*/
+    highlightEverything: true,
+      typerDataAttr     : 'data-typer-targets',
+      typerInterval     : 3000
+    },
+    highlight,
+    clearText,
+    backspace,
+    type,
+    spanWithColor,
+    clearDelay,
+    typeDelay,
+    clearData,
+    isNumber,
+  highlightEverything,
+    typeWithAttribute,
+    getHighlightInterval,
+    getTypeInterval,
+    typerInterval;
+
+  spanWithColor = function(color, backgroundColor) {
+    if (color === 'rgba(0, 0, 0, 0)') {
+      color = 'rgb(255, 255, 255)';
+    }
+
+    return $('<span></span>')
+      .css('color', color)
+      .css('background-color', backgroundColor);
+  };
+
+  isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  };
+
+  clearData = function ($e) {
+    $e.removeData([
+      'typePosition',
+      'highlightPosition',
+      'leftStop',
+      'rightStop',
+      'primaryColor',
+      'backgroundColor',
+      'text',
+      'typing'
+    ]);
+  };
+
+  type = function ($e) {
+    var
+      // position = $e.data('typePosition'),
+      text = $e.data('text'),
+      oldLeft = $e.data('oldLeft'),
+      oldRight = $e.data('oldRight');
+
+    // if (!isNumber(position)) {
+      // position = $e.data('leftStop');
+    // }
+
+    if (!text || text.length === 0) {
+      clearData($e);
+      return;
+    }
+
+
+    $e.text(
+      oldLeft +
+      text.charAt(0) +
+      oldRight
+    ).data({
+      oldLeft: oldLeft + text.charAt(0),
+      text: text.substring(1)
+    });
+
+    // $e.text($e.text() + text.substring(position, position + 1));
+
+    // $e.data('typePosition', position + 1);
+
+    setTimeout(function () {
+      type($e);
+    }, getTypeInterval());
+  };
+
+  clearText = function ($e) {
+    $e.find('span').remove();
+
+    setTimeout(function () {
+      type($e);
+    }, typeDelay());
+  };
+
+  highlight = function ($e) {
+    var
+      position = $e.data('highlightPosition'),
+      leftText,
+      highlightedText,
+      rightText;
+
+    if (!isNumber(position)) {
+      position = $e.data('rightStop') + 1;
+    }
+
+    if (position <= $e.data('leftStop')) {
+      setTimeout(function () {
+        clearText($e);
+      }, clearDelay());
+      return;
+    }
+
+    leftText = $e.text().substring(0, position - 1);
+    highlightedText = $e.text().substring(position - 1, $e.data('rightStop') + 1);
+    rightText = $e.text().substring($e.data('rightStop') + 1);
+
+    $e.html(leftText)
+      .append(
+        spanWithColor(
+            $e.data('backgroundColor'),
+            $e.data('primaryColor')
+          )
+          .append(highlightedText)
+      )
+      .append(rightText);
+
+    $e.data('highlightPosition', position - 1);
+
+    setTimeout(function () {
+      return highlight($e);
+    }, getHighlightInterval());
+  };
+
+  typeWithAttribute = function ($e) {
+    var targets;
+
+    if ($e.data('typing')) {
+      return;
+    }
+
+    try {
+      targets = JSON.parse($e.attr($.typer.options.typerDataAttr)).targets;
+    } catch (e) {}
+
+    if (typeof targets === "undefined") {
+      targets = $.map($e.attr($.typer.options.typerDataAttr).split(','), function (e) {
+        return $.trim(e);
+      });
+    }
+  
+  var i = 0;
+  setInterval(function () {
+    $e.typeTo(targets[i]);
+    i++;
+    if( i == targets.length) {
+      i=0;
+    }
+    }, typerInterval());
+    
+  
+  };
+
+  // Expose our options to the world.
+  $.typer = (function () {
+    return { options: options };
+  })();
+
+  $.extend($.typer, {
+    options: options
+  });
+
+  //-- Methods to attach to jQuery sets
+
+  $.fn.typer = function() {
+    var $elements = $(this);
+
+    return $elements.each(function () {
+      var $e = $(this);
+
+      if (typeof $e.attr($.typer.options.typerDataAttr) === "undefined") {
+        return;
+      }
+
+      typeWithAttribute($e);
+    });
+  };
+
+  $.fn.typeTo = function (newString) {
+    var
+      $e = $(this),
+      currentText = $e.text(),
+      i = 0,
+      j = 0;
+
+    if (currentText === newString) {
+      // console.log("Our strings are equal, nothing to type");
+      return $e;
+    }
+
+    if (currentText !== $e.html()) {
+      // console.error("Typer does not work on elements with child elements.");
+      return $e;
+    }
+
+    $e.data('typing', true);
+
+    if ($.typer.options.highlightEverything !== true) {
+      while (currentText.charAt(i) === newString.charAt(i)) {
+        i++;
+      }
+
+      while (currentText.rightChars(j) === newString.rightChars(j)) {
+        j++;
+      }
+    }
+
+    newString = newString.substring(i, newString.length - j + 1);
+
+    $e.data({
+      oldLeft: currentText.substring(0, i),
+      oldRight: currentText.rightChars(j - 1),
+      leftStop: i,
+      rightStop: currentText.length - j,
+      primaryColor: $e.css('color'),
+      backgroundColor: $e.css('background-color'),
+      text: newString
+    });
+
+    highlight($e);
+
+    return $e;
+  };
+
+  //-- Helper methods. These can one day be customized further to include things like ranges of delays.
+
+  getHighlightInterval = function () {
+    return $.typer.options.highlightSpeed;
+  };
+
+  getTypeInterval = function () {
+    return $.typer.options.typeSpeed;
+  },
+
+  clearDelay = function () {
+    return $.typer.options.clearDelay;
+  },
+
+  typeDelay = function () {
+    return $.typer.options.typeDelay;
+  };
+
+  typerInterval = function () {
+    return $.typer.options.typerInterval;
+  };
+  
+  highlightEverything = function () {
+    return $.typer.options.highlightEverything;
+  };
+  
+})(jQuery);
+
+$(function () {
+  $('.first [data-typer-targets]').typer();
+
+  //$.typer.options.typerInterval = 2000;
+
+  // $('.second [data-typer-targets]').typer();
+
+  /*$('.second [data-typer-targets]').typer2({
+    highlightEverything: false
+  });*/
+});
